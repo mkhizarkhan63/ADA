3df55bbff6896534adae0fc9ba8671bc329e73a4
Khizar Khan <46076164+mkhizarkhan63@users.noreply.github.com>
2021-07-27 00:40:45 -0700

    Add project files.

---
+++ ADA.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADA.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpGet("string")]
+        public string demo() {
+            return "hello world";
+        }
+    }
+}
---
+++ ADA.web/wwwroot/js/ajax.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    alert("helllo");
+    $.ajax({
+        url: "https://localhost:44317/api/Authentication/string",
+        type: "GET",
+        success: function (data) {
+            console.warn(data);
+        }, error: function (data) {
+            console.warn(data);
+        }
+
+
+
+    });
+
+});
+
+
+
