b8f9eb4ac257a7b9ee5c581935cf83b1bef4c8ba
hasnain ali <khasnainali8@gmail.com>
2021-08-23 14:20:15 +0500

    Admin DashBoard

--- ADA.web/Startup.cs
+++ ADA.web/Startup.cs
@@ -49,10 +49,22 @@ namespace ADA.web
 
             app.UseEndpoints(endpoints =>
             {
+
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+                endpoints.MapControllerRoute(
+                 name: "areas",
+                 pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+               );
+
+            
             });
+
+           
         }
     }
 }
